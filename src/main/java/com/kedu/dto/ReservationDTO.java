cf2a06c9919b97df1b5af57fc33b8a8505a0dd46
changgi8678 <a01086418679@gmail.com>
2024-09-12 12:28:57 +0900

    09_12_이슈해결

--- src/main/java/com/kedu/dto/ReservationDTO.java
+++ src/main/java/com/kedu/dto/ReservationDTO.java
@@ -4,7 +4,7 @@ import java.sql.Timestamp;
 
 public class ReservationDTO {
 	private int reservationId;
-    private int userId;
+    private String userId;
     private int storeSeq;
     private Timestamp reservationDate;
     private String reservationTime;
@@ -12,61 +12,10 @@ public class ReservationDTO {
     private String status;
     private Timestamp createdAt;
     private Timestamp updatedAt;
-	public int getReservationId() {
-		return reservationId;
-	}
-	public void setReservationId(int reservationId) {
-		this.reservationId = reservationId;
-	}
-	public int getUserId() {
-		return userId;
-	}
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-	public int getStoreSeq() {
-		return storeSeq;
-	}
-	public void setStoreSeq(int storeSeq) {
-		this.storeSeq = storeSeq;
-	}
-	public Timestamp getReservationDate() {
-		return reservationDate;
-	}
-	public void setReservationDate(Timestamp reservationDate) {
-		this.reservationDate = reservationDate;
-	}
-	public String getReservationTime() {
-		return reservationTime;
-	}
-	public void setReservationTime(String reservationTime) {
-		this.reservationTime = reservationTime;
-	}
-	public int getNumGuests() {
-		return numGuests;
-	}
-	public void setNumGuests(int numGuests) {
-		this.numGuests = numGuests;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public Timestamp getCreatedAt() {
-		return createdAt;
-	}
-	public void setCreatedAt(Timestamp createdAt) {
-		this.createdAt = createdAt;
-	}
-	public Timestamp getUpdatedAt() {
-		return updatedAt;
-	}
-	public void setUpdatedAt(Timestamp updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-	public ReservationDTO(int reservationId, int userId, int storeSeq, Timestamp reservationDate,
+	
+    
+    
+	public ReservationDTO(int reservationId, String userId, int storeSeq, Timestamp reservationDate,
 			String reservationTime, int numGuests, String status, Timestamp createdAt, Timestamp updatedAt) {
 		super();
 		this.reservationId = reservationId;
@@ -79,7 +28,117 @@ public class ReservationDTO {
 		this.createdAt = createdAt;
 		this.updatedAt = updatedAt;
 	}
-    
+
+
+
+	public int getReservationId() {
+		return reservationId;
+	}
+
+
+
+	public void setReservationId(int reservationId) {
+		this.reservationId = reservationId;
+	}
+
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+
+	public int getStoreSeq() {
+		return storeSeq;
+	}
+
+
+
+	public void setStoreSeq(int storeSeq) {
+		this.storeSeq = storeSeq;
+	}
+
+
+
+	public Timestamp getReservationDate() {
+		return reservationDate;
+	}
+
+
+
+	public void setReservationDate(Timestamp reservationDate) {
+		this.reservationDate = reservationDate;
+	}
+
+
+
+	public String getReservationTime() {
+		return reservationTime;
+	}
+
+
+
+	public void setReservationTime(String reservationTime) {
+		this.reservationTime = reservationTime;
+	}
+
+
+
+	public int getNumGuests() {
+		return numGuests;
+	}
+
+
+
+	public void setNumGuests(int numGuests) {
+		this.numGuests = numGuests;
+	}
+
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+
+
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+
 	public ReservationDTO() {
 		// TODO Auto-generated constructor stub
 	}
