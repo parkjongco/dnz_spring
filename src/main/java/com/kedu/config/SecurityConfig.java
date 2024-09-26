bc83a9235f014f873a5631ade1d81b15d6f7fb93
JongHo <ehdus232323@naver.com>
2024-09-26 20:20:36 +0900

    시큐리티

--- src/main/java/com/kedu/config/SecurityConfig.java
+++ src/main/java/com/kedu/config/SecurityConfig.java
@@ -39,55 +39,64 @@ public class SecurityConfig {
                             "http://192.168.1.172:3000"
                     ));
                     config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "*"));
-                    config.setAllowedMethods(Arrays.asList("*"));  // 모든 메서드를 명시적으로 허용
+                    config.setAllowedMethods(Arrays.asList("*"));  // 허용되는 HTTP 메서드 설정
                     config.setAllowCredentials(true);
                     return config;
                 }))
-                .csrf(AbstractHttpConfigurer::disable)  // CSRF 공격 방지 기능 비활성화
+                .csrf(AbstractHttpConfigurer::disable)  // CSRF 공격 방지 비활성화
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         // 인증 없이 접근 가능한 경로 설정
-                        .requestMatchers(HttpMethod.GET, "/store/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/maps/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/photos/store/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/uploads/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/menu/store/**").permitAll() // 가게 ID로 메뉴 조회 허용
-                        .requestMatchers(HttpMethod.GET, "/menu/**").permitAll() // 메뉴 ID로 메뉴 조회 허용
-                        .requestMatchers(HttpMethod.POST, "/menu/**").hasAnyRole("ADMIN", "OWNER")
-                        .requestMatchers(HttpMethod.PUT, "/menu/**").hasAnyRole("ADMIN", "OWNER")
-                        .requestMatchers(HttpMethod.DELETE, "/menu/**").hasAnyRole("ADMIN", "OWNER")
-                        .requestMatchers(HttpMethod.POST, "/replies/**").hasAnyRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/replies/**").permitAll()
-
-                        .requestMatchers(HttpMethod.POST, "/reviews/**").permitAll() // 리뷰 작성 허용
-                        .requestMatchers(HttpMethod.GET, "/reviews/**").permitAll() // 리뷰 조회 허용
-                        .requestMatchers(HttpMethod.PUT, "/reviews/**").permitAll() // 리뷰 수정 허용
-                        .requestMatchers(HttpMethod.DELETE, "/reviews/**").permitAll() // 리뷰 삭제 허용
-
-
-                        .requestMatchers("/reservation/**").permitAll() // 모든 사용자 접근 허용
-
-                        .requestMatchers("/api/activities/**").permitAll()
-                        .requestMatchers("/members/**").permitAll() // 모든 사용자에게 접근 허용
-                        .requestMatchers("/bookmark/**").hasAnyRole("ADMIN", "OWNER", "USER")
-
-                        .requestMatchers(HttpMethod.POST, "/api/posts/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/api/posts/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/api/posts/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
-
-                        .requestMatchers(HttpMethod.POST, "/registerOwner").hasAnyRole("ADMIN", "OWNER")
-                        .requestMatchers(HttpMethod.POST, "/registerUser").permitAll()  // 모든 사용자에게 접근 허용
-
-                        // 역할에 따른 접근 제어 추가
-                        .requestMatchers("/admin/**").hasRole("ADMIN")   // 관리자만 접근 가능
-                        .requestMatchers("/owner/**").hasAnyRole("ADMIN", "OWNER")   // 관리자 또는 점주 접근 가능
-                        .requestMatchers("/user/**").hasAnyRole("ADMIN", "OWNER", "USER")     // 관리자, 점주, 일반 사용자 접근 가능
-
-
+                                .requestMatchers(HttpMethod.GET, "/store/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/maps/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/auth/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/photos/store/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/uploads/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/menu/store/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/menu/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/menu/**").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.PUT, "/menu/**").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.DELETE, "/menu/**").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.POST, "/replies/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/replies/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/store/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/store/register").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.PUT, "/store/update").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.GET, "/store/info").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.GET, "/store/{storeSeq}/name").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/store/category/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/store/{storeId}").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/store/all").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/store/register").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.GET, "/store/info").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.PUT, "/store/update").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.GET, "/store/{storeId}/photos").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/store/{storeSeq}/photos").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/reviews/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/reviews/**").permitAll()
+                                .requestMatchers(HttpMethod.PUT, "/reviews/**").permitAll()
+                                .requestMatchers(HttpMethod.DELETE, "/reviews/**").permitAll()
+                                .requestMatchers("/reservation/**").permitAll()
+                                .requestMatchers("/api/activities/**").permitAll()
+                                .requestMatchers("/members/**").permitAll()
+                                .requestMatchers("/bookmark/**").hasAnyRole("ADMIN", "OWNER", "USER")
+                                .requestMatchers(HttpMethod.POST, "/api/posts/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/api/posts/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/api/posts/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/registerOwner").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers(HttpMethod.POST, "/registerUser").permitAll()
+// 공지사항 관련 접근 제어 추가
+                                .requestMatchers(HttpMethod.GET, "/store/{storeSeq}/notice").permitAll() // 특정 가게 공지사항 조회
+                                .requestMatchers(HttpMethod.GET, "/notice").hasAnyRole("ADMIN", "OWNER") // 가게 공지사항 조회
+                                .requestMatchers(HttpMethod.POST, "/notice").hasAnyRole("ADMIN", "OWNER") // 가게 공지사항 추가
+                                .requestMatchers(HttpMethod.PUT, "/notice").hasAnyRole("ADMIN", "OWNER") // 가게 공지사항 수정
+// 역할에 따른 접근 제어
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/owner/**").hasAnyRole("ADMIN", "OWNER")
+                                .requestMatchers("/user/**").hasAnyRole("ADMIN", "OWNER", "USER")
                         .anyRequest().authenticated()
                 )
                 // JWT 필터 추가
